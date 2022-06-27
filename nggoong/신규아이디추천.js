972021404b530587487e8cad0c2cd8036152874e
nggoong <nggoong@naver.com>
2022-06-27 14:45:15 +0900

    21 - 24 문제 커밋

---
+++ nggoong/신규아이디추천.js
@@ -0,0 +1,18 @@
+// 신규 아이디 추천
+// https://programmers.co.kr/learn/courses/30/lessons/72410
+
+function solution(new_id) {
+    var answer = '';
+    answer = new_id.toLowerCase()
+    .replace(/[^\w-_.]/g, '')
+    .replace(/\.{2,}/g, '.')
+    .replace(/^\.|\.$/g, '');
+    if(answer.length === 0) answer+='a';
+    answer = answer.slice(0, 15).replace(/\.$/g, '');
+    
+    const length = answer.length;
+    if(length <= 2) {
+        answer += answer[length-1].repeat(3-length);
+    }
+    return answer;
+}
