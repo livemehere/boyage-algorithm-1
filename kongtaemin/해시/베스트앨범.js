16cc81cb4a56fef8af8d742344f5e884ece78569
livemehere <rhdxoals@gmail.com>
2022-07-05 02:16:39 +0900

    베스트 앨범 2번쨰 풀이

--- kongtaemin/해시/베스트앨범.js
+++ kongtaemin/해시/베스트앨범.js
@@ -1,33 +1,33 @@
 function solution(genres, plays) {
   let answer = [];
-  let n = plays.length;
-  let arr = [];
-  let total = new Map();
+  let songs = [];
+  let records = {};
 
-  for (let i = 0; i < n; i++) {
-    let prev = total.get(genres[i]) || 0;
-    prev = parseInt(prev);
-    total.set(genres[i], [prev + plays[i], 0]);
-    arr.push({ id: i, genre: genres[i], play: plays[i] });
+  for (let i = 0; i < plays.length; i++) {
+    songs.push({
+      idx: i,
+      genre: genres[i],
+      play: plays[i],
+    });
+
+    let acc = records[genres[i]] ? records[genres[i]][0] : 0;
+    acc = acc + plays[i];
+    records[genres[i]] = [acc, 0];
   }
 
-  arr.sort((a, b) => {
+  songs.sort((a, b) => {
     if (a.play === b.play) {
-      return a.id < b.id ? -1 : 1;
-    } else {
-      return 0;
+      return a.idx - b.idx; // a > b = + b가 앞. 즉 적은게 앞으로 감. 오름차순
     }
-  }); // 내림차순
-  arr.sort((a, b) => (a.play > b.play ? -1 : 1)); // 재생순
-  arr.sort((a, b) =>
-    Number(total.get(a.genre)[0]) > Number(total.get(b.genre)[0]) ? -1 : 1
-  ); // 장르
+  });
 
-  arr.forEach((a) => {
-    const useCnt = total.get(a.genre)[1];
-    if (useCnt < 2) {
-      total.set(a.genre, [total.get(a.genre)[0], total.get(a.genre)[1] + 1]);
-      answer.push(a.id);
+  songs.sort((a, b) => b.play - a.play); // b > a = + b가 앞으로간다. 즉, 내림 차순
+  songs.sort((a, b) => records[b.genre][0] - records[a.genre][0]); // b > a = + b가 앞으로, 즉 내림차순
+
+  songs.forEach((s) => {
+    if (records[s.genre][1] < 2) {
+      answer.push(s.idx);
+      records[s.genre][1]++;
     }
   });
 
